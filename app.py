55133efbcc3fd9d2b7ca92edf689a6c99fe67691
Jules Bimana <saloonngosmart@gmail.com>
2025-11-09 22:44:45 +0100

    Prepared app for new hosting: Removed Render DB URL replace logic

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 # Gushyiraho Database URI: Render irazishaka muri Environment
 # Iyi irahita isoma DATABASE_URL twashyize kuri Render
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Kwemeza SQLAlchemy
